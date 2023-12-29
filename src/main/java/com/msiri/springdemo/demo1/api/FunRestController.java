fd8aa537e79c09e81080de1c22a0bc13bb93f32e
Menaka S <menaka442@gmail.com>
2023-12-29 12:42:01 +0530

    inject values from application.properties file

--- src/main/java/com/msiri/springdemo/demo1/api/FunRestController.java
+++ src/main/java/com/msiri/springdemo/demo1/api/FunRestController.java
@@ -1,13 +1,17 @@
 package com.msiri.springdemo.demo1.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${coach.name}")
+    private String coachName;
+
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return "Hello World! " + coachName;
     }
 }
