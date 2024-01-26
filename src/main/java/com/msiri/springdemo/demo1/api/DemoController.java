32dcfa70f6b51e0b97ddd1808ac300e9b1cea664
Menaka S <menaka442@gmail.com>
2024-01-26 23:45:22 +0530

    setter injection demo

--- src/main/java/com/msiri/springdemo/demo1/api/DemoController.java
+++ src/main/java/com/msiri/springdemo/demo1/api/DemoController.java
@@ -10,9 +10,15 @@ public class DemoController {
     //define a private field for the dependency
     private Coach myCoach;
 
+    /*
     //define a constructor for dependency injection
     @Autowired
     public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    } */
+
+    @Autowired
+    public void doSomeStuff (Coach theCoach) {
         myCoach = theCoach;
     }
 
