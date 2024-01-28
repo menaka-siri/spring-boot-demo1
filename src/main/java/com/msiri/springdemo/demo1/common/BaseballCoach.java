d38f46c5184ff5345560040a4691eed8d4702cdc
Menaka S <menaka442@gmail.com>
2024-01-28 12:02:28 +0530

    check beans lifecycle

--- src/main/java/com/msiri/springdemo/demo1/common/BaseballCoach.java
+++ src/main/java/com/msiri/springdemo/demo1/common/BaseballCoach.java
@@ -1,5 +1,7 @@
 package com.msiri.springdemo.demo1.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,19 @@ public class BaseballCoach implements Coach{
     public  BaseballCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
+
+    //define the init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    //define the destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30mins in batting practice";
