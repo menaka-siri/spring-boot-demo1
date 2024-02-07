4e10161de46adc8501de861f07a903ade2a04b78
Menaka S <menaka442@gmail.com>
2024-02-07 19:47:32 +0530

    add StudentNotFoundException

--- src/main/java/com/msiri/springdemo/demo1/api/StudentRestController.java
+++ src/main/java/com/msiri/springdemo/demo1/api/StudentRestController.java
@@ -2,10 +2,9 @@ package com.msiri.springdemo.demo1.api;
 
 import com.msiri.springdemo.demo1.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +32,22 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        if ( studentId >= theStudents.size() || studentId < 0) {
+            throw  new StudentNotFoundException(String.format("Student id %d not found!", studentId));
+        }
+
         return theStudents.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
